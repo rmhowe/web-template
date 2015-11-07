e53b95b3a9b49a498ee22b3c093dfa800afb27b8
rmhowe <rm.howe@outlook.com>
2015-11-07 12:01:03 +1100

    Removes some default styles and content

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,15 +1,9 @@
-require('font-awesome/css/font-awesome.css');
 require('../css/app.scss');
 
 require('babel-core/polyfill');
 
 document.addEventListener('DOMContentLoaded', () => {
-  let container = document.createElement('div');
-  let text = document.createElement('span');
-  text.textContent = "Hello, this is dog ";
-  let icon = document.createElement('i');
-  icon.className = "fa fa-paw";
-  container.appendChild(text);
-  container.appendChild(icon);
-  document.getElementById('app').appendChild(container);
+  let greeting = document.createElement('span');
+  greeting.textContent = "Hello, this is dog";
+  document.getElementById('app').appendChild(greeting);
 });
